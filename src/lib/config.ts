9563eefae2217c683c82ffff866e2d34d1b0afc9
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2024-07-05 13:01:17 +0300

    environment variable sveltekit native test

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,8 @@
 import { dev } from '$app/environment'
 import { base } from '$app/paths'
+import { PUBLIC_URL } from '$env/static/public'
 
 export const title = 'Ilkerflix'
 export const description = 'Ilkerflix'
-export const url = (dev ? 'http://localhost:5173' : process.env.PUBLIC_URL) + base
+export const url = (dev ? 'http://localhost:5173' : PUBLIC_URL) + base
+// process.env.PUBLIC_URL
