929ce5c406452979c47ce235a10cfd4bfe25625d
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2024-07-05 13:09:14 +0300

    PUBLIC_URL moved to .env file

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,6 @@
-import { dev } from '$app/environment'
 import { base } from '$app/paths'
 import { PUBLIC_URL } from '$env/static/public'
 
 export const title = 'Ilkerflix'
 export const description = 'Ilkerflix'
-export const url = (dev ? 'http://localhost:5173' : PUBLIC_URL) + base
-// process.env.PUBLIC_URL
+export const url = PUBLIC_URL + base
