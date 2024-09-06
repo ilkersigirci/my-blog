a48a37b8179e17bc2adaf888684797c5a4ffd0fe
İlker SIĞIRCI <39417966+ilkersigirci@users.noreply.github.com>
2024-09-06 16:35:17 +0300

    moved search bar to header and made toggle (#8)
    * moved search bar to header and made toggle
    
    * search modal moved to another file

--- src/lib/components/search/PageFindSearch.svelte
+++ src/lib/components/search/PageFindSearch.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte'
 	import { page } from '$app/stores'
 	import { PagefindUI } from '@pagefind/default-ui'
@@ -12,25 +12,33 @@
 			showImages: false,
 			showEmptyFilters: false,
 			autofocus: true,
+			// resetStyles: false,
 			// sort: { date: "desc" },
 			// bundlePath: "/pagefind/",
-			processResult: function (result) {
+			processResult: function (result: any) {
 				// Remove .html from result.url and result.raw_url
 				result.url = result.url.replace(/\.html$/, '')
 				result.raw_url = result.raw_url.replace(/\.html$/, '')
-
 				return result
 			}
 		})
-		pagefind.triggerSearch($page.url.searchParams.get('q'))
-		const search_input = document.querySelector('#search input')
 
-		search_input.addEventListener('input', (e) => {
-			const search_params = new URLSearchParams(window.location.search)
-			search_params.set('q', e.target.value)
-			const new_url = `${window.location.pathname}?${search_params.toString()}`
-			history.pushState(null, '', new_url)
-		})
+		const searchInput = document.querySelector('#search input') as HTMLInputElement
+		if (searchInput) {
+			searchInput.addEventListener('input', (e: Event) => {
+				const target = e.target as HTMLInputElement
+				const searchParams = new URLSearchParams(window.location.search)
+				searchParams.set('q', target.value)
+				const newUrl = `${window.location.pathname}?${searchParams.toString()}`
+				history.pushState(null, '', newUrl)
+			})
+		}
+
+		// Trigger search with current query parameter
+		const currentQuery = $page.url.searchParams.get('q')
+		if (currentQuery) {
+			pagefind.triggerSearch(currentQuery)
+		}
 	})
 </script>
 
@@ -38,14 +46,40 @@
 	<title>Search {title}</title>
 </svelte:head>
 
-<div id="search" />
+<div id="search" class="custom_pagefind_search" />
 
-<!-- <style is:global>
-	.dark {
-		--pagefind-ui-primary: #eeeeee;
-		--pagefind-ui-text: #eeeeee;
-		--pagefind-ui-background: #152028;
-		--pagefind-ui-border: #152028;
-		--pagefind-ui-tag: #152028;
+<style>
+	:root {
+		--pagefind-ui-scale: 0.8;
+		--pagefind-ui-primary: #393939;
+		--pagefind-ui-text: #393939;
+		--pagefind-ui-background: #ffffff;
+		--pagefind-ui-border: #eeeeee;
+		--pagefind-ui-tag: #eeeeee;
+		--pagefind-ui-border-width: 2px;
+		--pagefind-ui-border-radius: 8px;
+		--pagefind-ui-image-border-radius: 8px;
+		--pagefind-ui-image-box-ratio: 3 / 2;
 	}
-</style> -->
+	/* @media (prefers-color-scheme: dark) {
+		.custom_pagefind_search {
+			--pagefind-ui-scale: 1;
+			--pagefind-ui-primary: #034ad8;
+			--pagefind-ui-text: #ffffff;
+			--pagefind-ui-background: #ffffff;
+			--pagefind-ui-border: #d21f1f;
+			--pagefind-ui-tag: #20b84d;
+			--pagefind-ui-border-width: 2px;
+			--pagefind-ui-border-radius: 8px;
+			--pagefind-ui-image-border-radius: 8px;
+			--pagefind-ui-image-box-ratio: 3 / 2;
+			--pagefind-ui-font: sans-serif;
+			--pagefind-ui-search-input-background: #ffffff;
+		}
+	} */
+
+	/* .custom_pagefind_search {
+		width: 100%;
+		max-width: 600px;
+	} */
+</style>
--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,29 +1,60 @@
 <script lang="ts">
 	import { base } from '$app/paths'
-
-	import Toggle from './toggle.svelte'
 	import * as config from '$lib/config'
+	import { SearchModal } from '$lib/components/search'
+	import { Search } from 'lucide-svelte'
+	import Toggle from './toggle.svelte'
+	import { onMount } from 'svelte'
+
+	let showSearchModal = false
+
+	onMount(() => {
+		const handleKeydown = (event: KeyboardEvent) => {
+			if (event.ctrlKey && event.key === 'k') {
+				event.preventDefault()
+				toggleSearchModal()
+			}
+			// else if (event.key === 'Escape') {
+			// 	showSearchModal = false
+			// }
+		}
+
+		window.addEventListener('keydown', handleKeydown)
+
+		return () => {
+			window.removeEventListener('keydown', handleKeydown)
+		}
+	})
+
+	function toggleSearchModal() {
+		showSearchModal = !showSearchModal
+		if (!showSearchModal) {
+			// Reset the search query parameter when closing the modal
+			const url = new URL(window.location.href)
+			url.searchParams.delete('q')
+			history.pushState(null, '', url)
+		}
+	}
 </script>
 
 <nav>
-	<a href="{base}/" class="title">
-		<b>{config.title}</b>
-	</a>
-
+	<a href="{base}/" class="title">{config.title}</a>
 	<ul class="links">
+		<li><a href="{base}/about">About</a></li>
+		<li><a href="{base}/contact">Contact</a></li>
+		<li><a href="{base}/rss.xml" target="_blank">RSS</a></li>
 		<li>
-			<a href="{base}/about">About</a>
-		</li>
-		<li>
-			<a href="{base}/contact">Contact</a>
-		</li>
-		<li>
-			<a href="{base}/rss.xml" target="_blank">RSS</a>
+			<a href="/search" on:click|preventDefault={toggleSearchModal} aria-label="Open search">
+				<Search />
+			</a>
 		</li>
 	</ul>
-
 	<Toggle />
 </nav>
+
+{#if showSearchModal}
+	<SearchModal on:close={toggleSearchModal} />
+{/if}
 
 <style>
 	nav {
@@ -51,4 +82,22 @@
 			margin-block: 0;
 		}
 	}
+
+	/* .modal {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.5);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.modal > :global(.custom_pagefind_search) {
+		background: white;
+		padding: var(--size-7);
+		border-radius: var(--pagefind-ui-border-radius);
+	} */
 </style>
