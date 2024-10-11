5f5016eab8323d326a2fa23cea1f1e9780eedff2
Ben Allfree <ben@benallfree.com>
2024-10-11 09:48:47 -0700

    working

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,7 @@ import { addCopyButton } from 'shiki-transformer-copy-button'
 import remarkToc from 'remark-toc'
 import rehypeSlug from 'rehype-slug'
 import { enhancedImages, defaultResolverFactory } from 'mdsvex-enhanced-images'
+import { join } from 'path'
 
 const addCopyButtonOptions = {
 	// delay time from "copied" state back to normal state
@@ -39,9 +40,12 @@ const mdsvexOptions = {
 		}
 	},
 	remarkPlugins: [
-		enhancedImages({
-			resolve: (path) => defaultResolverFactory((path) => join('src', 'assets', 'images', path))
-		}),
+		[
+			enhancedImages,
+			{
+				resolve: defaultResolverFactory((path) => join('$img', path))
+			}
+		],
 		remarkUnwrapImages,
 		[remarkToc, { tight: true }]
 	],
@@ -57,6 +61,9 @@ const config = {
 		prerender: { handleHttpError: 'warn' },
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		alias: {
+			$img: 'src/assets/images'
 		}
 	}
 }
